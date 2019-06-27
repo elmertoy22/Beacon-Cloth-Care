d0509f6debc9978a2f7d633708fa94fbb1e0610e
unknown <elmertalucod3@gmail.com>
2019-06-27 14:51:27 +0800

    6th commit

--- superadmin/assets/partials/city/muntinlupa.php
+++ superadmin/assets/partials/city/muntinlupa.php
@@ -3,7 +3,8 @@
 <div class="panel with-nav-tabs">
     <div class="panel-heading">
         <ul class="nav nav-tabs">
-            <li class="active"><a href="#tab1primary" data-toggle="tab">Warehouse</a></li>
+            <li class="active"><a href="#tabprimary" data-toggle="tab">Overall Sales</a></li>
+            <li><a href="#tab1primary" data-toggle="tab">Warehouse</a></li>
             <li><a href="#tab2primary" data-toggle="tab">Branch 1</a></li>
             <li><a href="#tab3primary" data-toggle="tab">Branch 2</a></li>
             <li><a href="#tab4primary" data-toggle="tab">Corporate Account</a></li>
@@ -18,7 +19,107 @@
         </ul>
     </div>
     <div class="panel-body">
-        <div class="tab-content ">
+        <div class="tab-content">
+            <div class="tab-pane fade in active " id="tabprimary">
+                <div class="row">
+                    <div class="col-md-12">
+                        <input type="search" class="form-control" placeholder="Search">
+                    </div>
+                    <div class="col-md-12" style="margin-top:10px;">
+                        <table class="table table-striped">
+                            <thead class="bg-primary">
+                              <tr>
+                                <th style="color:white;" >Date Received</th>
+                                <th style="color:white;" >Job order #</th>
+                                <th style="color:white;" >name of customer</th>
+                                <th style="color:white;" >kilos/pcs</th>
+                                <th style="color:white;" >amount</th>
+                                <th style="color:white;" >pickup date</th>
+                                <th style="color:white;" >pickup time</th>
+                                <th style="color:white;" >date release</th>
+                                <th style="color:white;" >Deposit/payment</th>
+                                <th style="color:white;" >Received</th>
+                              </tr>
+                            </thead>
+                            <tbody align="center">
+                              <tr>
+                                <td>03/02/2019</td>
+                                <td>0081</td>
+                                <td>Juan dela Cruz</td>
+                                <td>3 kilos</td>
+                                <td>150.00</td>
+                                <td>03/04/2019</td>
+                                <td>3:02 pm</td>
+                                <td>03/04/2019</td>
+                                <td>Paid</td>
+                                <td>employee 1</td>
+                              </tr>
+                              <tr>
+                                <td>03/02/2019</td>
+                                <td>0081</td>
+                                <td>Juan dela Cruz</td>
+                                <td>3 kilos</td>
+                                <td>150.00</td>
+                                <td>03/04/2019</td>
+                                <td>3:02 pm</td>
+                                <td>03/04/2019</td>
+                                <td>Paid</td>
+                                <td>employee 1</td>
+                              </tr>
+                              <tr>
+                                <td>03/02/2019</td>
+                                <td>0081</td>
+                                <td>Juan dela Cruz</td>
+                                <td>3 kilos</td>
+                                <td>150.00</td>
+                                <td>03/04/2019</td>
+                                <td>3:02 pm</td>
+                                <td>03/04/2019</td>
+                                <td>Paid</td>
+                                <td>employee 1</td>
+                              </tr>
+                              <tr>
+                                <td>03/02/2019</td>
+                                <td>0081</td>
+                                <td>Juan dela Cruz</td>
+                                <td>3 kilos</td>
+                                <td>150.00</td>
+                                <td>03/04/2019</td>
+                                <td>3:02 pm</td>
+                                <td>03/04/2019</td>
+                                <td>Paid</td>
+                                <td>employee 1</td>
+                              </tr>
+                              <tr>
+                                <td>03/02/2019</td>
+                                <td>0081</td>
+                                <td>Juan dela Cruz</td>
+                                <td>3 kilos</td>
+                                <td>150.00</td>
+                                <td>03/04/2019</td>
+                                <td>3:02 pm</td>
+                                <td>03/04/2019</td>
+                                <td>Paid</td>
+                                <td>employee 1</td>
+                              </tr>
+                              <tr>
+                                <td>03/02/2019</td>
+                                <td>0081</td>
+                                <td>Juan dela Cruz</td>
+                                <td>3 kilos</td>
+                                <td>150.00</td>
+                                <td>03/04/2019</td>
+                                <td>3:02 pm</td>
+                                <td>03/04/2019</td>
+                                <td>Paid</td>
+                                <td>employee 1</td>
+                              </tr>
+                              
+                            </tbody>
+                          </table>
+                    </div>
+                </div>
+            </div>
             <div class="tab-pane fade in active " id="tab1primary">
                 <div class="row">
                     <div class="col-md-12">
--- superadmin/assets/partials/sales.php
+++ superadmin/assets/partials/sales.php
@@ -5,9 +5,10 @@
 <div class="panel with-nav-tabs">
     <div class="panel-heading">
         <ul class="nav nav-tabs">
-            <li class="active"><a href="#tab1primary" data-toggle="tab">Muntinlupa City</a></li>
-            <li><a href="#tab2primary" data-toggle="tab">Cavite City</a></li>
-            <li><a href="#tab3primary" data-toggle="tab">Davao City</a></li>
+            <li class="active"><a href="#tabprimarysales" data-toggle="tab">Overall Sales</a></li>
+            <li><a href="#tab1primarysales" data-toggle="tab">Muntinlupa City</a></li>
+            <li><a href="#tab2primarysales" data-toggle="tab">Cavite City</a></li>
+            <li><a href="#tab3primarysales" data-toggle="tab">Davao City</a></li>
             <!--<li class="dropdown">
                 <a href="#" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
                 <ul class="dropdown-menu" role="menu">
@@ -19,15 +20,70 @@
     </div>
     <div class="panel-body">
         <div class="tab-content">
-            <div class="tab-pane fade in active" id="tab1primary">
-                <?php
+            <div class="tab-pane fade in active" id="tabprimarysales">
+               <div class="row">
+                   <div class="col-md-6">
+                       <center>
+                           <select class="form-control">
+                               <option disabled selected>Select</option>
+                               <option>yearly</option>
+                               <option>monthly</option>
+                           </select>
+
+                           <div class="col-md-12" style="display:none;">
+                               <select class="form-control">
+                                   <option disabled selected>Select</option>
+                                   <option>January</option>
+                                   <option>February</option>
+                                   <option>March</option>
+                                   <option>April</option>
+                                   <option>May</option>
+                                   <option>June</option>
+                                   <option>July</option>
+                                   <option>August</option>
+                                   <option>September</option>
+                                   <option>October</option>
+                                   <option>November</option>
+                                   <option>December</option>
+                               </select>
+
+                           </div>
+                           <div class="col-md-12" style="display:none;">
+                               <select class="form-control">
+                                   <option disabled selected>Select</option>
+                                   <option>2015</option>
+                                   <option>2016</option>
+                                   <option>2017</option>
+                                   <option>2018</option>
+                                   <option>2019</option>
+                                   <option>2020</option>
+                                   <option>2021</option>
+                                   <option>2022</option>
+                                   <option>2023</option>
+                                   <option>2024</option>
+                                   <option>2025</option>
+                                   <option>2026</option>
+                                   <option>2027</option>
+                                   <option>2028</option>
+                                   <option>2029</option>
+                                   <option>2030</option>
+                               </select>
+
+                           </div>
+                        </center>
+                    </div>
+                </div>
+            </div>
+            <div class="tab-pane fade" id="tab1primarysales">
+             <?php
                     include('city/muntinlupa.php');
                 ?>
+            
             </div>
-            <div class="tab-pane fade" id="tab2primary"></div>
-            <div class="tab-pane fade" id="tab3primary"></div>
-            <div class="tab-pane fade" id="tab4primary">Primary 4</div>
-            <div class="tab-pane fade" id="tab5primary">Primary 5</div>
+            <div class="tab-pane fade" id="tab2primarysales"></div>
+            <div class="tab-pane fade" id="tab3primarysales"></div>
+            <div class="tab-pane fade" id="tab4primarysales">Primary 4</div>
+            <div class="tab-pane fade" id="tab5primarysales">Primary 5</div>
         </div>
         </div>
     </div>
